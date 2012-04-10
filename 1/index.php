88a6689ca33bb2dc0d63c46e30e295a32280bf3f
tokune <tokune@gmail.com>
2012-04-10 19:52:08 +0800

    add project to github

---
+++ 1/index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>还原短地址</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+</head>
+
+<body>
+  
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $url = get_final_url($_POST["long_url"]);
+    echo "还原出来的地址:<a href=\"$url\">".$url."</a>";
+}
+else {
+?>
+<form action="" method="post">
+还原短地址<p>
+比如:http://t.cn/zOCoDoL<p> 
+<input name="long_url" type="txt" /> <input name="submit" type="submit" value="unshort" />
+</form>
+
+
+<?
+}
+/**
+ * get_redirect_url()
+ * Gets the address that the provided URL redirects to,
+ * or FALSE if there's no redirect. 
+ *
+ * @param string $url
+ * @return string
+ */
+function get_redirect_url($url){
+    $redirect_url = null; 
+
+    $url_parts = @parse_url($url);
+    if (!$url_parts) return false;
+    if (!isset($url_parts['host'])) return false; //can't process relative URLs
+    if (!isset($url_parts['path'])) $url_parts['path'] = '/';
+
+    $sock = fsockopen($url_parts['host'], (isset($url_parts['port']) ? (int)$url_parts['port'] : 80), $errno, $errstr, 30);
+    if (!$sock) return false;
+
+    $request = "HEAD " . $url_parts['path'] . (isset($url_parts['query']) ? '?'.$url_parts['query'] : '') . " HTTP/1.1\r\n"; 
+    $request .= 'Host: ' . $url_parts['host'] . "\r\n"; 
+    $request .= "Connection: Close\r\n\r\n"; 
+    fwrite($sock, $request);
+    $response = '';
+    while(!feof($sock)) $response .= fread($sock, 8192);
+    fclose($sock);
+
+    if (preg_match('/^Location: (.+?)$/m', $response, $matches)){
+        if ( substr($matches[1], 0, 1) == "/" )
+            return $url_parts['scheme'] . "://" . $url_parts['host'] . trim($matches[1]);
+        else
+            return trim($matches[1]);
+
+    } else {
+        return false;
+    }
+
+}
+
+/**
+ * get_all_redirects()
+ * Follows and collects all redirects, in order, for the given URL. 
+ *
+ * @param string $url
+ * @return array
+ */
+function get_all_redirects($url){
+    $redirects = array();
+    while ($newurl = get_redirect_url($url)){
+        if (in_array($newurl, $redirects)){
+            break;
+        }
+        $redirects[] = $newurl;
+        $url = $newurl;
+    }
+    return $redirects;
+}
+
+/**
+ * get_final_url()
+ * Gets the address that the URL ultimately leads to. 
+ * Returns $url itself if it isn't a redirect.
+ *
+ * @param string $url
+ * @return string
+ */
+function get_final_url($url){
+    $redirects = get_all_redirects($url);
+    if (count($redirects)>0){
+        return array_pop($redirects);
+    } else {
+        return $url;
+    }
+}?>
+ <br/> <a href="http://sae.sina.com.cn" target="_blank"><img src="http://static.sae.sina.com.cn/image/poweredby/poweredby.png" title="Powered by Sina App Engine"></a>
+</body>
+</html> 
